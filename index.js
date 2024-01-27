549b6c05040117638a2d3188220f0676281101f3
unknown <saadbinhossain002@gmail.com>
2024-01-27 10:06:25 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+function CreatFrom(){
+    const textOne = document.getElementById("first-input").value;
+    const textTwo = document.getElementById("second-input").value;
+    const textThree = document.getElementById("third-input").value;
+    const textFour = document.getElementById("four-input").value;
+
+    console.log(textOne , textTwo,textThree,textFour);
+}
